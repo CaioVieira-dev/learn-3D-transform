a9388072043209db9d7b41f7e8ec8c75d2fcb6e3
Caio Vieira <caiocesarav0208@gmail.com>
2021-07-20 13:42:41 -0300

    docs: :bulb: add comments to FunctionsOverview
    add comments explaining some things about 3d functions

--- src/pages/FunctionsOverview/styles.ts
+++ src/pages/FunctionsOverview/styles.ts
@@ -8,6 +8,42 @@ display: grid;
   grid-template-rows: 1fr 1fr 1fr 1fr ; 
   gap: 30px 30px; 
 `
+/** 
+ 3D transform functions
+
+The three big functions are rotate, translate and scale,
+each of then can be used as a shorthand or specifying
+which axis will be transformed.
+
+Axis and translate:
+ X is the width axis, using with translate it will
+ move the object left or right relative to the it's 
+ initial position.
+    X => _
+ 
+ Y is the height axis, using with translate it will
+ move the object up or down relative to it's initial
+ position.
+    Y => |
+ 
+ Z is the depth axis, using with translate it will
+ move the object closer or further to the perspective.
+    Z => / 
+
+About rotate:
+    It's quite counterintuitive but X axis rotates around
+the horizontal X axis. So it looks like pushing or pulling
+from the top of the object, instead of rotating around the
+horizontal X axis.
+Using the shorthand notation: "rotate3d(0, 1, 0, 60deg)", X,Y,Z,angle
+       
+About scale:
+    With scale3d you specify the amount of scaling applied to
+each axis of the object.
+Using shorthand notation: "scale3d(2,0.5,3)", X,Y,Z
+
+**/
+
 
 export const Scene = styled.div`
 perspective: 400px;
