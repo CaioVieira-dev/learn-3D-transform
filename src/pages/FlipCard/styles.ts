575d9c67977f98c1821d012e4aa57c126b4f89db
Caio Vieira <caiocesarav0208@gmail.com>
2021-07-20 15:59:38 -0300

    feat: :bulb: comment FlipCard styles
    create a comment explaining how to make the flip card animation

--- src/pages/FlipCard/styles.ts
+++ src/pages/FlipCard/styles.ts
@@ -1,5 +1,30 @@
 import styled from "styled-components";
+/**
+    How to Flip a Card
+    
+    Pattern: 
+        scene, object, faces
 
+    Structure used: 
+        <Scene>
+            <Card>
+                <CardFace></CardFace>
+                <CardFace></CardFace>
+            </Card>
+        </Scene>
+
+    Steps:
+        1 - Create a Scene with the perspective
+        2 - Create a Card with position relative
+                and transform-style preserve-3d
+        3 - Create a CardFace with position absolute
+                and backface-visibility hidden
+        4 - Rotate backface with rotateY(180deg)
+        5 - Trigger a transform in card with 
+                rotateY(180deg)
+        optional - Make a nice style for the CardFace
+
+ */
 
 export const Scene = styled.div`
 width: 200px;
