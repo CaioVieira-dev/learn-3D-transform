290a9438f8b1efd784deb33107eff5c15edabae8
Caio Vieira <caiocesarav0208@gmail.com>
2021-07-20 10:14:58 -0300

    feat: :sparkles: implement react-router-dom
    create react-router-dom structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { GlobalStyle } from './styles/styles'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
 import { Basics } from './pages/Basics'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Basics />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Basics} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
