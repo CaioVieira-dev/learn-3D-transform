d9634487ca05e1edc9bee7f83c1e72022dec0ae7
Caio Vieira <caiocesarav0208@gmail.com>
2021-07-20 11:32:20 -0300

    feat: :sparkles: implement transform functions example
    create panels illustrating transform functions

--- src/pages/FunctionsOverview/index.tsx
+++ src/pages/FunctionsOverview/index.tsx
@@ -5,7 +5,28 @@ export function FunctionsOverview() {
     return (
         <Container>
             <Scene>
-                <Panel />
+                <Panel bgColor="blue" transform="rotX" inverted={false} >RotateX 45deg</Panel>
+            </Scene>
+            <Scene>
+                <Panel bgColor="blue" transform="rotX" inverted={true} >RotateX -45deg</Panel>
+            </Scene>
+            <Scene>
+                <Panel bgColor="green" transform="rotY" inverted={false}>RotateY 45deg</Panel>
+            </Scene>
+            <Scene>
+                <Panel bgColor="green" transform="rotY" inverted={true} >RotateY -45deg</Panel>
+            </Scene>
+            <Scene>
+                <Panel bgColor="purple" transform="rotZ" inverted={false} >RotateZ 45deg</Panel>
+            </Scene>
+            <Scene>
+                <Panel bgColor="purple" transform="rotZ" inverted={true} >RotateZ -45deg</Panel>
+            </Scene>
+            <Scene>
+                <Panel bgColor="red" transform="translZ" inverted={false} >TranslateZ 50px</Panel>
+            </Scene>
+            <Scene>
+                <Panel bgColor="red" transform="translZ" inverted={true} >TranslateZ -50px</Panel>
             </Scene>
         </Container>
     )
--- src/pages/FunctionsOverview/styles.ts
+++ src/pages/FunctionsOverview/styles.ts
@@ -5,17 +5,47 @@ max-width: 500px;
 width:100%;
 display: grid; 
   grid-template-columns: 1fr 1fr; 
-  grid-template-rows: 1fr 1fr 1fr; 
-  gap: 0px 0px; 
+  grid-template-rows: 1fr 1fr 1fr 1fr ; 
+  gap: 30px 30px; 
 `
 
 export const Scene = styled.div`
 perspective: 400px;
 `
+const panelSize = `200px`
 
-export const Panel = styled.div`
-width: 200px;
-height: 200px;
-background-color: purple;
-transform: rotateY(60deg);
+const applyTransform = (type: "rotY" | "rotX" | "rotZ" | "translX" | "translY" | "translZ", isInverted: boolean) => {
+    switch (type) {
+        case "rotY":
+            return `rotateY(${isInverted ? "-" : ""}45deg)`
+        case "rotX":
+            return `rotateX(${isInverted ? "-" : ""}45deg)`
+        case "rotZ":
+            return `rotateZ(${isInverted ? "-" : ""}45deg)`
+        case "translX":
+            return `translateX(${isInverted ? "-" : ""}200px)`
+        case "translY":
+            return `translateY(${isInverted ? "-" : ""}50px)`
+        case "translZ":
+            return `translateZ(${isInverted ? "-" : ""}50px)`
+
+
+    }
+}
+
+export const Panel = styled.div<{
+    transform: "rotY" | "rotX" | "rotZ" | "translX" | "translY" | "translZ",
+    inverted: boolean,
+    bgColor: string
+}>`
+width: ${panelSize};
+height: ${panelSize};
+color:white;
+text-align: center;
+line-height: ${panelSize};
+border: 5px solid black;
+font-size: 24px;
+
+background-color: ${props => props.bgColor};
+transform: ${props => applyTransform(props.transform, props.inverted)}
 `
