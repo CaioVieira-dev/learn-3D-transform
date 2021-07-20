bec75b56efe81539628df164231f79b08ff97d58
Caio Vieira <caiocesarav0208@gmail.com>
2021-07-20 14:15:44 -0300

    feat: :sparkles: implement card flip animation
    make flip on hover

--- src/pages/FlipCard/index.tsx
+++ src/pages/FlipCard/index.tsx
@@ -1,10 +1,13 @@
-import { Scene } from './styles'
+import { Scene, Card, CardFace } from './styles'
 
 export function FlipCard() {
 
     return (
         <Scene>
-
+            <Card>
+                <CardFace face="front">Face 1</CardFace>
+                <CardFace face="back">Face 2</CardFace>
+            </Card>
         </Scene>
     )
 }
--- src/pages/FlipCard/styles.ts
+++ src/pages/FlipCard/styles.ts
@@ -2,11 +2,32 @@ import styled from "styled-components";
 
 
 export const Scene = styled.div`
+width: 200px;
+height: 300px;
 perspective: 400px;
 `
 export const Card = styled.div`
+width: 100%;
+height:100%;
+position: relative;
+transition: transform 1s;
+transform-style: preserve-3d;
+
+:hover&{
+    transform: rotateY(180deg);
+}
+
 
 `
-export const CardFace = styled.div`
+export const CardFace = styled.div<{ face: "front" | "back" }>`
+position:absolute;
+width:100%;
+height:100%;
+backface-visibility: hidden;
+background-color: ${props => props.face === "front" ? "green" : "orange"};
+${props => props.face === "back" && "transform:rotateY(180deg);"};
+color:white;
+line-height: 300px;
+text-align: center;
 
 `
