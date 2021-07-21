25b48039654bb2229a94d8dbb428058c3b73f8c7
Caio Vieira <caiocesarav0208@gmail.com>
2021-07-21 12:51:14 -0300

    feat: 💫 add 3d carousel make a 3d carousel made with 3d transform

--- src/pages/Carousel/index.tsx
+++ src/pages/Carousel/index.tsx
@@ -1,9 +1,19 @@
-import { Scene } from './styles'
+import { Scene, CarouselContainer, Cell } from './styles'
 
 export function Carousel() {
     return (
         <Scene>
-
+            <CarouselContainer>
+                <Cell bgColor="red">1</Cell>
+                <Cell bgColor="blue">2</Cell>
+                <Cell bgColor="green">3</Cell>
+                <Cell bgColor="turquoise">4</Cell>
+                <Cell bgColor="orange">5</Cell>
+                <Cell bgColor="cyan">6</Cell>
+                <Cell bgColor="purple">7</Cell>
+                <Cell bgColor="pink">8</Cell>
+                <Cell bgColor="darkgoldenrod">9</Cell>
+            </CarouselContainer>
         </Scene>
     )
 }
--- src/pages/Carousel/styles.ts
+++ src/pages/Carousel/styles.ts
@@ -1,4 +1,42 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Scene = styled.div`
+    width: 210px;
+    height: 140px;
+    position: relative;
+    perspective: 1000px;
+`
+export const CarouselContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    transform-style: preserve-3d;
+    transform: translateZ(-288px);
+`
+const generateTransformRotation = (cellAmount: number) => {
+    let styles = '';
+    for (let i = 0; i < cellAmount + 1; i++) {
+        styles += `
+        :nth-of-type(${i})&{
+            transform:rotateY(${i * 40}deg) translateZ(288px);
+        }
+        `
+    }
+    return css`${styles}`;
+}
+
+export const Cell = styled.div<{ bgColor: string }>`
+    position: absolute;
+    width: 190px;
+    height: 120px;
+    line-height: 120px;
+    text-align: center;
+    left: 10px;
+    top: 10px;
+    font-size: 24px;
+    color: white;
+    opacity: 0.8;
+    background-color: ${props => props.bgColor};
+    ${generateTransformRotation(9)};
+
 `
