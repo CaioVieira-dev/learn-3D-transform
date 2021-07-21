7eb3f1aa26600621f5be09370503c703753f4edb
Caio Vieira <caiocesarav0208@gmail.com>
2021-07-21 15:24:03 -0300

    feat: :construction: work on Header component
    make nav bar and style it

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,21 @@
-import { StyledHeader } from './styles'
+import { StyledHeader, Title, Navigation, NavigationItem } from './styles'
+import { useState } from 'react'
 
 export function Header() {
+    const [isOpen, setIsOpen] = useState(false)
+    function toggleNav() {
+        setIsOpen(!isOpen);
+    }
     return (
         <StyledHeader>
-
+            <Title onClick={toggleNav}>Efeito: spin cube</Title>
+            <Navigation childAmount={5} className={isOpen ? "visible" : ""}>
+                <NavigationItem to="/">Basics</NavigationItem>
+                <NavigationItem to="/functions">Functions</NavigationItem>
+                <NavigationItem to="/animation/cube-spin">Cube Spin</NavigationItem>
+                <NavigationItem to="/animation/box-spin">Box Spin</NavigationItem>
+                <NavigationItem to="/animation/flip-card">Flip Card</NavigationItem>
+            </Navigation>
         </StyledHeader>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,11 +1,68 @@
 import styled from "styled-components";
+import { Link } from 'react-router-dom'
+
+const headerHeight = `56px`;
+const bgHue = `252`;
+const bgTitleSaturation = `71%`;
+const bgTitleBrightness = `25%`;
+const bgNavBrightness = `10%`;
+const bgNavItemBrightness = `45%`;
+
+const getHeaderHeightNumber = () => {
+    return Number(headerHeight.slice(0, headerHeight.length - 2));
+}
+
+const configTop = (childAmount: number) => {
+    const n = childAmount + 1;
+    return `top:-${n * getHeaderHeightNumber() + 1}px`
+}
+
+
 
 export const StyledHeader = styled.header`
 max-width: 500px;
 width:clamp(360px,100%,420px);
-height: 56px;
-background-color:#aaa;
+height: ${headerHeight};
+
 position: fixed;
 top: 0;
+text-align: center;
+cursor: pointer;
+z-index: 1000;
+`
+
+export const Title = styled.h3`
+color:white;
+font-size: 24px;
+font-weight: bold;
+line-height:${headerHeight};
+
+background: hsl(${bgHue},${bgTitleSaturation},${bgTitleBrightness});
+z-index:10;
+`
+export const Navigation = styled.nav<{ childAmount: number }>`
+width: 100%;
+display: flex;
+flex-direction: column;
+
+position:relative;
+transition:0.5s;
+${props => configTop(props.childAmount)};
+.visible&{ 
+   top:0;
+}
+z-index: -1;
 
 `
+export const NavigationItem = styled(Link)`
+z-index: -10;
+width:100%;
+height:  ${headerHeight};
+background-color: hsl(${bgHue},${bgTitleSaturation},${bgNavItemBrightness});
+color:white;
+text-decoration: none;
+line-height:${headerHeight};
+border-top: 1px solid hsl(${bgHue},${bgTitleSaturation},${bgNavBrightness});;
+z-index:1;
+`
+
