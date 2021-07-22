940d8596c999f6aad38c20c0c142a01e8e99aa29
Caio Vieira <caiocesarav0208@gmail.com>
2021-07-22 09:11:36 -0300

    feat: :sparkles: include link to carousel page
    include link to carousel page in navigation conponent

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,12 +11,13 @@ export function Header() {
     return (
         <StyledHeader>
             <Title onClick={toggleNav}>{pageName}</Title>
-            <Navigation childAmount={5} className={isOpen ? "visible" : ""}>
+            <Navigation childAmount={6} className={isOpen ? "visible" : ""}>
                 <NavigationItem onClick={toggleNav} to="/">Basics</NavigationItem>
                 <NavigationItem onClick={toggleNav} to="/functions">Functions</NavigationItem>
                 <NavigationItem onClick={toggleNav} to="/animation/cube-spin">Cube Spin</NavigationItem>
                 <NavigationItem onClick={toggleNav} to="/animation/box-spin">Box Spin</NavigationItem>
                 <NavigationItem onClick={toggleNav} to="/animation/flip-card">Flip Card</NavigationItem>
+                <NavigationItem onClick={toggleNav} to="/animation/carousel">Carousel</NavigationItem>
             </Navigation>
         </StyledHeader>
     )
